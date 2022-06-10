ed274cc3387e5ecc3cc836bdb69e74b05ac2ad92
Alexis LEON <leonalex@free.fr>
2022-06-10 14:18:40 +0200

    changement API trad + corr mise en page

--- scripts/api.js
+++ scripts/api.js
@@ -1,16 +1,9 @@
 const api_lyrics = "https://api.lyrics.ovh/v1/";
 
-const google_api_key = "AIzaSyARtkUkEvaC8P0FoR6_AQKmQL_jKdqcpL0";
+const google_api_key = "AIzaSyBkwmvDCw0IM5bT9-_aoAS1xO5xD4n-xmI";
 const api_ytb = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&key=" + google_api_key + "&q="
 
-const api_traduction = "https://translated-mymemory---translation-memory.p.rapidapi.com/api/get?langpair=en%7Cfr&mt=1&onlyprivate=0&de=a%40b.c&q=";
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '5c6f710156msh2e4976576a6f568p1ba809jsncb8decf50f08',
-		'X-RapidAPI-Host': 'translated-mymemory---translation-memory.p.rapidapi.com'
-	}
-};
+const api_traduction = "https://lingva.ml/api/v1/en/fr/";
 
 async function find_music(q) {
   var query = encodeURI(api_ytb + q);
@@ -26,10 +19,13 @@ async function find_lyrics(artist, title) {
   return data.lyrics;
 }
 
-async function find_translate(lyrics){
-  const res = await fetch(api_traduction + lyrics, options)
-	const data = await res.json();
-  return data.responseData.translatedText;
+async function find_translate(str){
+  if (str) {
+    const res = await fetch(api_traduction + str)
+    const data = await res.json();
+    return data.translation;
+  }
+  return "";
 }
 
 async function get_video(video) {
@@ -45,6 +41,17 @@ async function get_lyrics(video) {
   return find_lyrics(res[0], res[1]);
 }
 
+async function get_translate(lyrics) {
+  let trad = "";
 
+  let split_lyrics = lyrics.split(/(?:\r\n|\n)/g);
+
+  for (const str of split_lyrics) {
+    const res = await find_translate(str);
+    trad += res + "<br>";
+  }
+
+  return trad;
+}
 
 
