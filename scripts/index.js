609d6243d8210eadd915145272b909f6a8349e73
02DL <95748958+02DL@users.noreply.github.com>
2022-06-14 13:46:57 +0200

    redirection

--- scripts/index.js
+++ scripts/index.js
@@ -7,5 +7,11 @@ const inputSong = document.querySelector("#songName");
 //on crée un évènement sur le submit du formulaire
 searchForm.addEventListener('submit', () => {
     // A FAIRE : doit redirect sur la même window et non en ouvrir une autre
-    window.open("http://127.0.0.1:5500/song.html?song=" + inputSong.value); // on passe en parametre de l'url les informations à traiter dans l'autre page
+   // window.open("http://127.0.0.1:5500/song.html?song=" + inputSong.value); // on passe en parametre de l'url les informations à traiter dans l'autre page
 })
+
+function redirectSong()
+{
+window.location.href = "song.html?song=" + inputSong.value;    
+return false;
+}
