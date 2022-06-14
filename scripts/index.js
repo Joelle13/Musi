d3e67708f00440b2c782d4dc82cfa1d703317fd0
Alexis LEON <leonalex@free.fr>
2022-06-14 23:47:34 +0200

    suppr commentaires

--- scripts/index.js
+++ scripts/index.js
@@ -3,13 +3,6 @@ const searchForm = document.querySelector("#searchForm");
 // On récupère l'input
 const inputSong = document.querySelector("#songName");
 
-
-//on crée un évènement sur le submit du formulaire
-searchForm.addEventListener('submit', () => {
-    // A FAIRE : doit redirect sur la même window et non en ouvrir une autre
-   // window.open("http://127.0.0.1:5500/song.html?song=" + inputSong.value); // on passe en parametre de l'url les informations à traiter dans l'autre page
-})
-
 function redirectSong()
 {
 window.location.href = "song.html?song=" + inputSong.value;    
